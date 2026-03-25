09e9f973e34720168f752e89c94035afdf2347f9
Lokvento <lokvento311@gmail.com>
2026-03-25 16:52:14 +0100

    Kompletní zadání podle 2. úkolu

--- src/main/java/pro1/swingComponents/MainFrame.java
+++ src/main/java/pro1/swingComponents/MainFrame.java
@@ -2,22 +2,26 @@ package pro1.swingComponents;
 
 import pro1.drawingModel.*;
 import pro1.drawingModel.Rectangle;
-import pro1.utils.ColorUtils;
+// import pro1.utils.ColorUtils;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFrame extends JFrame {
     private DisplayPanel displayPanel;
-    private int x;
-    private int y;
-    private String color = "#000000";
-
-    public void setColor(String color) {
-        this.color = color;
-    }
+    // private int x;
+    // private int y;
+    // private String color = "#000000";
+    private List<Point> circles = new ArrayList<>();
+    private int radius = 10;
+    private boolean hide = false;
+    // public void setColor(String color) {
+    // this.color = color;
+    // }
 
     public MainFrame() {
         this.setTitle("PRO1 Drawing");
@@ -35,21 +39,72 @@ public class MainFrame extends JFrame {
         this.displayPanel.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                MainFrame.this.x = e.getX();
-                MainFrame.this.y = e.getY();
-                MainFrame.this.showExample();
+                // MainFrame.this.x = e.getX();
+                // MainFrame.this.y = e.getY();
+                // MainFrame.this.showExample();
+                circles.add(new Point(e.getX(), e.getY()));
+                updateCanvas();
             }
         });
     }
 
-    public void showExample(){
-        MainFrame.this.displayPanel.setDrawable(this.example());
+    public void setRadius(int radius) {
+        this.radius = radius;
+        updateCanvas();
+
     }
 
-    private Drawable example() {
-        var d1 = new Ellipse(0, 0, 150, 250, this.color);
-        var d2 = new Text(0, 0, this.color);
-        var d3 = new Line(0, 50,170,170,3, this.color);
-        return new Group(new Drawable[]{d1, d2, d3}, this.x, this.y, 40, 1, 1);
+    public void setHide(boolean hide) {
+        this.hide = hide;
+        updateCanvas();
     }
+
+    public void resetCanvas() {
+        this.circles.clear();
+        updateCanvas();
+    }
+
+    private void updateCanvas() {
+        List<Drawable> elements = new ArrayList<>();
+        if (this.circles.size() >= 2 && !this.hide) {
+            int minX = Integer.MAX_VALUE;
+            int minY = Integer.MAX_VALUE;
+            int maxX = Integer.MIN_VALUE;
+            int maxY = Integer.MIN_VALUE;
+            for (Point p : this.circles) {
+                if (p.x < minX)
+                    minX = p.x;
+                if (p.y < minY)
+                    minY = p.y;
+                if (p.x > maxX)
+                    maxX = p.x;
+                if (p.y > maxY)
+                    maxY = p.y;
+            }
+            int rectX = minX;
+            int rectY = minY;
+            int rectWidth = maxX - minX;
+            int rectHeight = maxY - minY;
+            elements.add(new Rectangle(rectX, rectY, rectWidth, rectHeight, "#FF0000"));
+        }
+        int diameter = 2 * this.radius;
+        for (Point p : this.circles) {
+            int topLeftX = p.x - this.radius;
+            int topLeftY = p.y - this.radius;
+            elements.add(new Ellipse(topLeftX, topLeftY, diameter, diameter, "#000000"));
+        }
+        Drawable[] elementsArray = elements.toArray(new Drawable[0]);
+        Group main = new Group(elementsArray, 0, 0, 0, 1, 1);
+        this.displayPanel.setDrawable(main);
+    }
+    // public void showExample(){
+    // MainFrame.this.displayPanel.setDrawable(this.example());
+    // }
+
+    // private Drawable example() {
+    // var d1 = new Ellipse(0, 0, 150, 250, this.color);
+    // var d2 = new Text(0, 0, this.color);
+    // var d3 = new Line(0, 50,170,170,3, this.color);
+    // return new Group(new Drawable[]{d1, d2, d3}, this.x, this.y, 40, 1, 1);
+    // }
 }
--- src/main/java/pro1/swingComponents/OptionsPanel.java
+++ src/main/java/pro1/swingComponents/OptionsPanel.java
@@ -1,45 +1,68 @@
 package pro1.swingComponents;
 
-import pro1.utils.ColorUtils;
+// import pro1.utils.ColorUtils;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class OptionsPanel extends JPanel
-{
+public class OptionsPanel extends JPanel {
     private final MainFrame parent;
-    private JSlider rSlider;
-    private JSlider gSlider;
-    private JSlider bSlider;
+    // private JSlider rSlider;
+    // private JSlider gSlider;
+    // private JSlider bSlider;
+    private JSlider radiusSlider;
+    private JCheckBox hideCheckBox;
+    private JButton resetBtn;
 
     public OptionsPanel(MainFrame parent) {
         this.parent = parent;
         this.setPreferredSize(new Dimension(200, 0));
-        JButton newColorBtn = new JButton("Náhodná barva");
-        this.add(newColorBtn);
-        newColorBtn.addActionListener(
-                (e)-> {
-                    this.parent.setColor(ColorUtils.randomColor());
-                    this.parent.showExample();
-                }
-        );
-        this.rSlider = new JSlider(0,255,0);
-        this.gSlider = new JSlider(0,255,0);
-        this.bSlider = new JSlider(0,255,0);
-        this.add(this.rSlider);
-        this.add(this.gSlider);
-        this.add(this.bSlider);
-        this.rSlider.addChangeListener((e)->this.sliderChanged());
-        this.gSlider.addChangeListener((e)->this.sliderChanged());
-        this.bSlider.addChangeListener((e)->this.sliderChanged());
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.add(new JLabel("Poloměr kružnice"));
+        this.radiusSlider = new JSlider(1, 100, 10);
+        this.add(this.radiusSlider);
+        this.radiusSlider.addChangeListener((e) -> {
+            this.parent.setRadius(this.radiusSlider.getValue());
+
+        });
+
+        this.hideCheckBox = new JCheckBox("Skrýt obdelník");
+        this.add(this.hideCheckBox);
+        this.hideCheckBox.addActionListener((e) -> {
+            this.parent.setHide(this.hideCheckBox.isSelected());
+
+        });
+
+        this.resetBtn = new JButton("Reset");
+        this.add(this.resetBtn);
+        this.resetBtn.addActionListener((e) -> {
+            this.parent.resetCanvas();
+        });
+        // JButton newColorBtn = new JButton("Náhodná barva");
+        // this.add(newColorBtn);
+        // newColorBtn.addActionListener(
+        // (e)-> {
+        // this.parent.setColor(ColorUtils.randomColor());
+        // this.parent.showExample();
+        // }
+        // );
+        // this.rSlider = new JSlider(0,255,0);
+        // this.gSlider = new JSlider(0,255,0);
+        // this.bSlider = new JSlider(0,255,0);
+        // this.add(this.rSlider);
+        // this.add(this.gSlider);
+        // this.add(this.bSlider);
+        // this.rSlider.addChangeListener((e)->this.sliderChanged());
+        // this.gSlider.addChangeListener((e)->this.sliderChanged());
+        // this.bSlider.addChangeListener((e)->this.sliderChanged());
     }
 
-    private void sliderChanged(){
-        this.parent.setColor(ColorUtils.color(
-                this.rSlider.getValue(),
-                this.gSlider.getValue(),
-                this.bSlider.getValue()
-        ));
-        this.parent.showExample();
-    }
+    // private void sliderChanged(){
+    // this.parent.setColor(ColorUtils.color(
+    // this.rSlider.getValue(),
+    // this.gSlider.getValue(),
+    // this.bSlider.getValue()
+    // ));
+    // this.parent.showExample();
+    // }
 }
